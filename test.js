c39fb325fb10539fdb10de2bfa11b1c74296693c
malkeetsunam4-designjavascript <malkeetsunam4@gmail.com>
2026-09-17 18:20:00 +0530

    contain config done

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
+const account_Id=14156
+var account_Password="12345"
+let account_Email="@Arashdeepkaur12344gmail.com"
+account_City="jaipur"
+
+// never prefer to use var and nothing
+account_Password="4545454"
+account_Email="malkeet4@1234gmail.com"
+account_City="bengaluru"
+// now we have to investigate it 
+console.table([account_Id,account_City,account_Email,account_Password]);
+
 
