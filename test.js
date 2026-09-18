86948e1f85b5e9aeac9d96d832d6b83ac90f6cbd
malkeetsunam4-designjavascript <malkeetsunam4@gmail.com>
2026-09-18 17:47:48 +0530

    container configration done

--- test.js
+++ test.js
@@ -9,5 +9,12 @@ account_Email="malkeet4@1234gmail.com"
 account_City="bengaluru"
 // now we have to investigate it 
 console.table([account_Id,account_City,account_Email,account_Password]);
+// "use strict"; it will run all our code in newer version of js 
+// alert(3+3) it will not work becuse we are working in node js its standartd are different
+let Name="arashdeep kaur"
+let age= "19"
+let isloggedin="true"
+let state= "null"
+console.log(typeof"state");
 
 
